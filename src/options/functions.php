db64014ce88b31c919bbba02d84e94206d3ae1c7
hncore <hncore@icloud.com>
2025-04-02 04:12:38 +0700

    Update

--- src/options/functions.php
+++ src/options/functions.php
@@ -2,12 +2,12 @@
 // Coder By HNCore
 if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
 ?>
-HR+cPmGFlIWlwmKFDhSe2t3SYoSB17EGdAI/4E0Qe0v2L5L6+cP7EqNxffBZH+jOMM2C7H7QeyxB
-Paso4IUqhRqbQzvHoglgNbdid0bASChYbV/PeflGc4/PPJBje2kdy/2Cj01uDYpviQJYkTMY0zYU
-2YnlkoHvmqwh8QMVjSK777XzrwMFh85aqISqts6LIte6s33TYM0NGdkmAeKLcT0p+aIVq0IA27HJ
-G4esNyjr/HDGOiGXoJX5lNAHmA5DyZQcSbPWRICuhlQ4dakBL3KXYLGXiWA8buzb0MbUYEZPoZkW
-38Mhbaa9i/Y/kllrbVwYvrBUPZuia+8Fjf+20UXMMozz5xAOfhJFgy0ElTBfdOjCde+uH05HboD1
-GYEx7JX8VPBjuoGSc7iVcYHkwMOG6eApM1yDdaGK1xWEOIWMBQn7HMgBbdu4miPrXxJEK0eMnFOf
-cbAnAcVxiHMcvbiWDBqbvULFmxozKiM80+u4JYq+ygkYXv6d9t/scWO0DKkbYZckwgSFZCUuvMNQ
-I88bNAptoOxg0uZl+glbYMiqGCRM2r135dglTGGCxXZhR97uynUGuBBR06Fu/uJxAI24gRbDzvHh
-OMsxnq8fd0mDi2PEONMha35A0eFMU7N4M/IWSNfDdW==
+HR+cPqiVQUbg+r7FSwVyoiv4tQEbeSCoGJF4FO+uFGojyhbXaTbmxqx0460gUKyQRxJVH7WTNXRP
+W/cPUlrrn0UB5OZ2tx6MiU63WqRl+hyRlAivL3Emu2/9405TT8+UqqVESl7tDSgjWS3XTg8aRlDz
+4X7BzngbCMZatsjt2ubFBMXgMwoFceJcC3wR+zVSfyLbPYDAEF4s3SHtP7+0fgzs+YIZ2VBZ9zLt
+Ybwjm3ESbB1HissU7bo1Oin6u+Udwmh+e1j3oU4/yJ1MdMTaMtlud3c8d1Hl/G4GVPj4GHsaqnzg
+3yfEzDPh2FDErWtXgCH8IWyIKJqqxlTcNRpsYVFj9zb6Bbff8NURbmnXdkDEPKdt55BKNReOqDfh
+WKP94V/ovzevsfSh77cRRWF2f847n9a5YgCcN/3Z9NCk+iV3v5aPb3HkTz+oRvoOQLfjwozqZbIn
+W5zrptsGq+bdPFjQqdRgzmG1ycSWaIaVyNyBj+31Ocx3XyRcjVKeBA3d4YMXYwIAjog63o5ErzfJ
+iSdbOzDmB0rR7N0dmWu54YBtN4prvOase1ktysRK2RBx6l0w9DNwlvbyk5gQ6S1GdRc833OV4Anc
+xpeOjgClQPRhTHRERlkb4y/RRg+pm7omHW==

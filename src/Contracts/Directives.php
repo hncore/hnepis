f7e54354b8c3e655b7cdb0666057fa023c8b47fe
hncore <hncore@icloud.com>
2025-04-04 18:13:36 +0700

    Update

--- src/Contracts/Directives.php
+++ src/Contracts/Directives.php
@@ -2,16 +2,16 @@
 // Coder By HNCore
 if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
 ?>
-HR+cPorBSBwrhqYoGFBbdPHne1LWw8LUTV4bBAou3H5mwVfThT7cOwGryxUhDcPEB57+8hc2XZva
-qEWZeePdoSuGc5splOgWb4LKMk3tYU5mXfOf+QngxePHKMqrtpMTm4nRmvTeX8RJaykMJLv/2lms
-3d0HUdb6xt2j07/tpsWl00Eib0rnNJKrJKI7/LKg3Ds1ZA8ZhAzHuhtw386pm+1Y/6rT6SKKYVT7
-hIs1kvADZuyfh32rGeVEY7Q0apy1wmufSlP05/2JWDEfQt6cL+A+Hh6MyqbhFf9uJE4h7CBCpLKW
-Fkfcmwky0srRa+5DxCj3LILP3TMHGzIiNbDcg/olb1nbC8GZbODIY+QQj68KQ/5U1L2B9c5R6UOY
-jFsU4LmPdH7nra2+LGa6OnDX/H6Ki8ThTiEXV9ebBTVhdIzg9+XpTM3A6bFPFxVqMjLFd7TNmUkU
-sEjfh/df2eT535vvE2Z7zJBD82pqON2/mhyqg48+IV2B0jeUvjoUEdA60fMpLr5BEVuEni/Iqm0p
-PhYZQiE2b1kDnw2Jary2P/z41nNWk/8kA2QPP8PvTHab1bKUCXq9LB+0h818A006Aj94lwNGTWPz
-bVqJ3CuG7XwQKSY8YNOT/9CRInJPrQPwzAVdYVy/uAiGeKIT+wi6J5LuhEaVFK1shsK62SCPLJXf
-/HcAr6a//1RcuXbpsQEof+n3aZT3yTd4mqx+SuneujM90zXg6/SMvY5PW4jubB1Zjskbo9u0EAAc
-uBjfdGgjLcKcfADUd9m3L7XOLn7YliB4s8QFrWuf2XzyCKjif2Eb43Fn94Ei68D7bOGPEwZQUSTt
-2ZhYBTjuMMutSxbIYEcndMm8tlyNxaShspjkMLXXH2h2jYj0yN5W8pHCDVCmrvw+8fshhgy9kHVN
-dT4=
+HR+cPtGP9wj7yfkC7g2oYDhd4652kzJ688HxDyXuw7hvjROfAx3NTgplm6El/n6xkaJLj8WAPHCo
++bwzekOfQrbVjBJF+Zf0JQRWM8Sef1OiBl31FV80wXpeADHXgdBvmHYZwMu4koaBC+I6jeIdeuoh
+15FHOS+vIfktZwICq1t9p1hY7s3+m3vraWn8l0+vD96rv9czbZzEzehQq7FMD7E7RZTUj6f1xrYv
+fYSE92TOWbeZRMNGn7lmu/OTnYWIbgesA5ohinPdkDt4x51TEFzeFx32xL7GRLpi1UupV0fOa0iu
+XlkfBHbeGv7goTm/rheu8KtaJioUx9E82+d4XAOEZMPdMjl7Tes9dUCzzpPZvtUDWysSu5FQ+hBa
+a/6vuS1lnulFReaG7UvGa7OrWI/6POj52C5YkR2lxvwS17xyCQ/B5eotH4J8Ydsg2z/GvtZD/U7f
+kMuqOTCGABfELeUz3qSwquOc9mPnj8mCanZuigQCdM3rMUlMbcC+lLA9RjoTsILBGexm9LFtCOOc
+IideSFA1X8zvL9nCgk52L9805S3+QF9TcQR1iO/h3XRERhTDemlW7sIeYY+cVV4DWqOKB6rVZgaB
+AoOG2NFl9ARWgKQgTc68icSUzYANzQJZ6BoXfNj+P1/Y/vs9MV0SJtgr0XSLj5rxPOXWrq2TDe8M
+ydazqqRJ+x8TqkL5A0O3+R+X6cFPfU3AE0qlmS4rLEXPzKoMgbDxRPiOpvYH0duUJpFGj6fj/2Lm
+qgmjM4NuOBblaMoMaOcZQJOGWwTb9wowL5lY2QRXXVO/yqmCuJAuGWqvWmTk4ljJNT6ltwzJmzMP
+N6Gcs2CVWaY8P1nDXa5fSk8oX0QPPtgvDLDR7j1n6Fe8TNIWrvSI6pg/ktIQGCpAnFTvjwOE5QB0
+ueZO
--- src/options/functions.php
+++ src/options/functions.php
@@ -2,12 +2,12 @@
 // Coder By HNCore
 if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
 ?>
-HR+cPrKnQUQH15BAR8dynPwGk0qORpzRDVUJGi8++CQJLmKaymdRbr9PVsERvlwinvTbJPEWUegK
-irBlx6sdUBJDGWB3R3PHJbgW5ht/vIpw4d3USsxAfYxJnsMoUMt6gqM2HVnUBJfJ4oBYYYjNIQzE
-QiFNxp+U2umS8tydY7almAsRsajFa/AhU+HZ95nf9wPxoYzuw/WLyN6D729YQ07bgbu4iyLwIr7H
-UT89ROm4VnprXfMhughkcu7Cn0zrx4RMVW6x61Vmau3JgMjnfbVYlaQnblEsQqqxnVf/R8mNn7jj
-6aZg5VG5TUVtijXcrSRz5M5fdLIPQoaB4jkdAd8rBCBeV2z6uCzeXTH5DEMYGUKpcAQHq93l99/r
-AL+LM8tJmONyHy1MBVaIAqK/yy/Kb838sWzGRqPnnsCiuzR96qHEbALv5U4eEPDb4yyGhiHV2e4Z
-Mxangqr0RFYbphziX8QZrkdNG3wPOhXj44kt6BUu8VyNQSA/InXNUNbQ9kHAbbB/W8LSVpN13+YM
-vaeBi1rCZ2C34p8oSvN9GoVAitY0QTG8Sc3qd3+1hQltZvhgQGomNnmDeC5+0G1za4B8uqxbFvgs
-nTy12sO+hdtPWprHQhJEZ/Ll4G6+hlHuYPa=
+HR+cPmzIIWyYf4uXE+hVB3xfEV3oef4wQ1sTlSjEnV+0g1wuEWJJk+sIZ0Kx/8BPdlotpBlI/+uu
+sXFa0N+a08M1GYaDqAHV5CqjHi1gSGMBos7tlhNwNN3EHf9eTYIA3Su06kFBVRdrEAxVgcdfPStM
+C1s4JmWfx9ge/YA5QeslZYY9meRSxfy75egP76ho7l+fOC2dMaHztHaGJ5cn0zmRBYY/SYl3HFO3
+5apyy3IBqAbDDeNimQ0uIUPcfx5wAp18CfSmpHPdkDt4x51TEFzeFx32xL73QXXXMwwAyA0BkDbG
+W0MgLHHvEOHoYcw7VVLWctmv2IjsIMTY7vN21D+IvirtssTWDHMtVYMxJJ5aMI+ZGtmJQdXfxXzz
+o1WhDj5M2V31p4qRxFVnYbvttd/m7fwiPyq1CGVKqa5o0z8wEbIlXoPskGkvruRscC0Yx4xHJYJn
+C0wSo5LKI7s/kLIMmWNn1uLA78PVHKhjQin58/XnrL2xKWEkfxwNtIz/40Aq+Jq9GFZMhMvr9Ddn
+TatwU+w+k93/sPLurd8X4SVJCgnXCbStb9aLkpfGPf6vz8Nb2s9GtvYqb5/oR2SKMsfsWokZLPjX
+sOmbdd2BR1o8rYnf80sbG4qKSaVYRGCEgXPsQvK=
